fd7d8daf2db47fc8e50508b6bc9d9dd6632662fc
Benson Chau <bchau3440@gmail.com>
2018-10-20 22:32:23 -0400

    added data uri

--- meme_swap.py
+++ meme_swap.py
@@ -1,16 +1,26 @@
+import os
+
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask import jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def meme_swap():
     return render_template("meme_swap.html")
 
-@app.route('/', methods=['POST'])
-def meme_swap_post():
-    pass
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        filename = request.form['file']
+
+        print(filename)
+
+        return jsonify(request.form['file'])
+
+    return render_template("meme_snap.html")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
